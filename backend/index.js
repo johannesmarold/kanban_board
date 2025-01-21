b14e0904f1bace527e64567d3e98231cd9803107
johannesmarold <maroldjohannes@gmail.com>
2025-01-21 17:58:48 +0100

    updated comments

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,6 @@ const path = require('path')
 // Data initialization
 ///////////////////////////
 
-//TODO: implement (see 6.1.1)
-// serialize tags.json into a string and store it in the variable tags
-// serialize columns.json into a string and store it in the variable columns
 const tags = require('./data/tags.json');
 const columns = require('./data/columns.json');
 
@@ -52,20 +49,8 @@ function findAndDeleteTask(wantedId) {
 // Server setup
 ///////////////////////////
 
-//TODO: implement (see 6.1.2)
 const app = express();
 const port = 3000;
-
-/*
-let allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', "*");
-  res.header('Access-Control-Allow-Methods', "*");
-  next();
-}
-
-app.use(allowCrossDomain);
-*/
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
@@ -85,15 +70,12 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 // CRUD operations
 ///////////////////////////
 
-//TODO: implement (see 6.1.3 - 6.1.9)
-// 6.1.3
 app.get('/api/counter', (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.write(JSON.stringify({"taskIdCounter" : taskIdCounter}));
   res.end();
 });
 
-//6.1.4
 app.get('/api/tags', (req, res) => {
   console.log("GET request");
   res.writeHead(200, {'Content-Type': 'application/json'});
@@ -101,16 +83,13 @@ app.get('/api/tags', (req, res) => {
   res.end();
 });
 
-//6.1.5
 app.get('/api/columns', (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.write(JSON.stringify(columns));
   res.end();
 });
 
-//6.1.6
 app.post('/api/tasks', jsonParser, (req, res) => {
-  //if case for empty or wrong body can be ignored
   console.log("POST request");
   newID = taskIdCounter;
   taskIdCounter++;
@@ -125,7 +104,6 @@ app.post('/api/tasks', jsonParser, (req, res) => {
   res.end();
 });
 
-//6.1.7
 app.put('/api/tasks/:id', jsonParser, (req, res) => {
   console.log("PUT request for editing");
   let foundTask = findTask(req.params.id);
@@ -142,7 +120,6 @@ app.put('/api/tasks/:id', jsonParser, (req, res) => {
   }
 });
 
-//6.1.8
 app.delete('/api/tasks/:id', jsonParser, (req, res) => {
   console.log("DELETE request");
   let deleted = findAndDeleteTask(req.params.id);
@@ -156,7 +133,6 @@ app.delete('/api/tasks/:id', jsonParser, (req, res) => {
   }
 });
 
-//6.1.9
 app.put('/api/move-task/:id', jsonParser, (req, res) => {
   console.log("PUT request for moving");
   let movableTask = findAndDeleteTask(req.params.id);
@@ -176,6 +152,5 @@ app.put('/api/move-task/:id', jsonParser, (req, res) => {
 // Start the server
 ///////////////////////////
 
-//TODO: implement (see 6.1.2)
 app.listen(port, () => console.log(`Express server listening on port ${port}...`));
 ///////////////////////////
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,6 @@ const title = 'My Kanban Board'
 
 /**
  * Calls the showModal() function in Modal.vue, assuming the component is registered and referenced.
- * DO NOT MODIFY.
  */
 function showModal(task = null, columnId = null) {
     if (modalRef.value) {
@@ -22,7 +21,6 @@ function showModal(task = null, columnId = null) {
 
 /**
  * Calls showModal() in editing mode.
- * DO NOT MODIFY.
  * @param taskId
  */
 function triggerEdit(taskId) {
@@ -37,11 +35,10 @@ function triggerEdit(taskId) {
 }
 
 ////////////////////////////////////////////////////////////////
-// API calls below, only modify the content of the functions, not their signature! //
+// API calls below
 ////////////////////////////////////////////////////////////////
 
 async function loadTags() {
-    //TODO: implement (see 6.2 / 6.1.4)
     await fetch('/api/tags', {
         method: 'GET'})
     .then(res => {return res.json()})
@@ -50,7 +47,6 @@ async function loadTags() {
 }
 
 async function loadColumns() {
-    //TODO: implement (see 6.2 / 6.1.5)
     await fetch('/api/columns', {
         method: 'GET',
     })
@@ -60,7 +56,6 @@ async function loadColumns() {
 }
 
 async function createTask(columnId, taskTitle, taskText, taskTags) {
-    //TODO: implement (see 6.2 / 6.1.6)
     let newTask = {
         column : columnId,
         title : taskTitle,
@@ -78,7 +73,6 @@ async function createTask(columnId, taskTitle, taskText, taskTags) {
 }
 
 async function editTask(taskId, taskTitle, taskText, taskTags) {
-    //TODO: implement (see 6.2 / 6.1.7)
     let editedTask = {
         title : taskTitle,
         text : taskText,
@@ -94,7 +88,6 @@ async function editTask(taskId, taskTitle, taskText, taskTags) {
 }
 
 async function deleteTask(taskId) {
-    //TODO: implement (see 6.2 / 6.1.8)
     await fetch('/api/tasks/'+taskId, {
         method: 'DELETE',
     })
@@ -103,7 +96,6 @@ async function deleteTask(taskId) {
 }
 
 async function moveTask(taskId, newColumnId) {
-    //TODO: implement (see 6.2 / 6.1.9)
     await fetch('/api/move-task/'+taskId, {
         method: 'PUT',
         headers: {'Content-Type': 'application/json',},
@@ -114,14 +106,12 @@ async function moveTask(taskId, newColumnId) {
 }
 
 onMounted(() => {
-    // DO NOT MODIFY
     loadTags()
     loadColumns()
 })
 </script>
 
 <template>
-    <!-- DO NOT MODIFY -->
     <Header :title="title" @show-modal="showModal" />
     <Board :columns="columns" @move-task="moveTask" @trigger-edit="triggerEdit" @delete-task="deleteTask"/>
     <Modal :tags="tags" :columns="columns" ref="modalRef" @create-task="createTask" @edit-task="editTask"/>
